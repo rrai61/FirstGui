ac910e679240a6fe2038981d14c17730f562a43d
ritu <ritu@10.30.11.144>
2016-04-12 16:37:28 -0500

    completed temperature lab

---
+++ src/firstgui/FirstGui.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstgui;
+
+/**
+ *
+ * @author ritu
+ */
+public class FirstGui {
+
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Create and display the form */
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                new MainWindow().setVisible(true);
+//            }
+//        });
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TemperatureConverter().setVisible(true);
+            }
+        });
+    }
+    
+}
